fa6d2a757450f983b7608f7f17877d490f935047
Ruchi <r144@my.centennialcollege.ca>
2019-02-03 22:19:06 -0500

    added ExperssJS support

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'about' });
+});
+
+/* GET products page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'products' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'services' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'contact' });
 });
 
 module.exports = router;
